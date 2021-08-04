3a88636f319365d843044edcd6e9c05412e37c39
jinhw12 <jinhw12@naver.com>
2021-08-04 16:27:04 +0900

    VideoPlayer other videos 버그 수정

--- src/components/UI/EachVideo3.js
+++ src/components/UI/EachVideo3.js
@@ -23,8 +23,10 @@ function EachVideo3({ video, setCurrentVideo, setComments }) {
         `https://www.googleapis.com/youtube/v3/commentThreads?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&textFormat=plainText&part=snippet&videoId=${video_id}&maxResults=15`
       )
       .then((res) => {
-        const comments = res.data.items.map(comment => comment.snippet.topLevelComment.snippet.textDisplay);
-        setCurrentVideo(video);
+        const comments = res.data.items.map(
+          (comment) => comment.snippet.topLevelComment.snippet.textDisplay
+        );
+        setCurrentVideo(myVideo);
         setComments(comments);
       });
   };
